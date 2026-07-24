e09d6905551da67c42d8b524702d0cb417410cdd
Indrayana-creator <indrayanawidhi24@gmail.com>
2026-07-24 09:58:53 +0700

    Hapus routes/TAMBAHAN_*.php
    Isinya cuma catatan "tambahin baris ini ke web.php" yang sudah lama
    digabung ke routes/web.php, jadi file ini cuma sampah.

--- routes/TAMBAHAN_web.php
+++
@@ -1,8 +0,0 @@
-// Tambahin baris-baris ini ke file routes/web.php yang udah ada
-// (jangan replace seluruh file, cukup tambahin di bagian bawah)
-
-use App\Http\Controllers\ImportController;
-
-Route::get('/import', [ImportController::class, 'form']);
-Route::post('/import/pekerja', [ImportController::class, 'pekerja'])->name('import.pekerja');
-Route::post('/import/petugas-it', [ImportController::class, 'petugasIt'])->name('import.petugasIt');
